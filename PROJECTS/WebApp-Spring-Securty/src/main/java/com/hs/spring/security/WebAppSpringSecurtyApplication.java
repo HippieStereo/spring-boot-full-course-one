4a45af06299f7184287592d4a7fdc0df4e5b10b5
João Luís <stereobroo@yahoo.com>
2021-02-08 14:58:09 +0000

    Create Project
    Spring Web Security

---
+++ PROJECTS/WebApp-Spring-Securty/src/main/java/com/hs/spring/security/WebAppSpringSecurtyApplication.java
@@ -0,0 +1,13 @@
+package com.hs.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpringSecurtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSpringSecurtyApplication.class, args);
+	}
+
+}
