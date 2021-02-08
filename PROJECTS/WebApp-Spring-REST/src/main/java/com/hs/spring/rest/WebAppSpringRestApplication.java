f9399d1cd16e7e0162c59e8a681f831370135d4b
João Luís <stereobroo@yahoo.com>
2021-02-08 14:20:23 +0000

    New Project
    Spring Data Rest

---
+++ PROJECTS/WebApp-Spring-REST/src/main/java/com/hs/spring/rest/WebAppSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.hs.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSpringRestApplication.class, args);
+	}
+
+}
---
+++ PROJECTS/WebApp-Spring-REST/src/test/java/com/hs/spring/rest/WebAppSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.spring.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
