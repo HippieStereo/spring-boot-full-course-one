9a6ee426b365e4d0f5dce131eedc087d76f3415f
João Luís <stereobroo@yahoo.com>
2021-02-08 14:11:17 +0000

    CRUD -> Jpa

--- PROJECTS/WebApp-Forms/src/main/java/com/hs/demo/FormController.java
+++ PROJECTS/WebApp-Forms/src/main/java/com/hs/demo/FormController.java
@@ -1,8 +1,15 @@
 package com.hs.demo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -53,9 +60,55 @@ public class FormController {
 	
 	@RequestMapping("/customers")
 	@ResponseBody
-	public String allCustomers(){
+	public List<Customer> allCustomers(){
 		
-		return repo.findAll().toString();
+		return repo.findAll();
+		
+	}
+	
+	@RequestMapping("/customers/{cid}")
+	@ResponseBody
+	public Optional<Customer> customerById(@PathVariable int cid){
+		
+		return repo.findById(cid);
+		
+	}
+	
+	@PostMapping("/customers")
+	public Customer customerNew(@RequestBody Customer customer){
+		
+		repo.save(customer);
+		
+		return customer;
+		
+	}
+	
+	@DeleteMapping("/customers/{cid}")
+	public Customer customerDeletById(@PathVariable int cid){
+		
+		Customer customer = repo.getOne(cid);
+
+		repo.delete(customer);
+		
+		return customer;
+		
+	}
+	
+	@PutMapping("/customers")
+	public Customer customerUpdate(@RequestBody Customer customer){
+		
+		repo.save(customer);
+		
+		return customer;
+		
+	}
+	
+	@PutMapping(path="/customersJson", consumes= {"application/json"})
+	public Customer customerUpdateJson(@RequestBody Customer customer){
+		
+		repo.save(customer);
+		
+		return customer;
 		
 	}
 	
